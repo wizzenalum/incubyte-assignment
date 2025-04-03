4a2abad40bc5211c761af3ebd0846169e934ba5e
Wizzenalum <sikri.ghanshyam@gmail.com>
2025-04-04 00:11:36 +0530

    modify add method to support multiple delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -7,8 +7,16 @@ module StringCalculator
   private
 
   def self.parse_add_input(input)
+    return parse_add_input_delimited(input) if input.start_with?('//')
     input_with_comma_separator = input.gsub('\n', ',')
     tokens = input_with_comma_separator.split(',')
+    tokens.map { |token| token.to_i}
+  end
+
+  def self.parse_add_input_delimited(input)
+    delimiter_token, numbers_token = input.split('\n')
+    delimiter = delimiter_token[2..]
+    tokens = numbers_token.split(delimiter)
     tokens.map { |token| token.to_i}
   end
 end
