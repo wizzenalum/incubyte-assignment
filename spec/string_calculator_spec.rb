fe1ace324438b6029c002ed96f00fd3f23b229a9
Wizzenalum <sikri.ghanshyam@gmail.com>
2025-04-03 23:43:44 +0530

    spec for add method to  support  newline is separator

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,8 +27,23 @@ RSpec.describe StringCalculator do
       let(:array) { Array.new(Random.rand*20).map{|num| (Random.rand*100).to_i}}
       let(:input) {array.join(', ')}
       it 'return the sum of numbers' do
-        p input
         expect(described_class.add(input)).to be(array.sum)
+      end
+    end
+
+    context 'when input string has comma or newline separated numbers' do
+      context 'when only newline separates the numbers' do
+        let(:input) { '20\n30\n50\n'}
+        it 'return sum of  numbers' do
+          expect(described_class.add(input)).to be(100)
+        end
+      end
+
+      context 'when comma and newline separates the numbers' do
+        let(:input) { '20, 30\n50'}
+        it 'return sum of  numbers' do
+          expect(described_class.add(input)).to be(100)
+        end
       end
     end
   end
