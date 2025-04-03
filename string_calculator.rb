fcf0d4c69fda0df300bc9facb1423293f8b17c81
Wizzenalum <sikri.ghanshyam@gmail.com>
2025-04-03 23:53:06 +0530

    modify add method to  support  newline and comma as separator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,14 @@
 module StringCalculator
   def self.add(input)
-    tokens = input.split(',')
-    numbers = tokens.map { |token| token.to_i}
+    numbers = parse_add_input(input)
     numbers.sum
+  end
+
+  private
+
+  def self.parse_add_input(input)
+    input_with_comma_separator = input.gsub('\n', ',')
+    tokens = input_with_comma_separator.split(',')
+    tokens.map { |token| token.to_i}
   end
 end
