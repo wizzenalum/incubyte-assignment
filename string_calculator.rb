67c32223d36032abeee52920a3a36b171e8fab1b
Wizzenalum <sikri.ghanshyam@gmail.com>
2025-04-04 01:31:56 +0530

    refactor StringCalculator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,25 +1,28 @@
 module StringCalculator
   def self.add(input)
-    numbers = parse_add_input(input)
-    validate_add_numbers(numbers)
+    numbers = parse_input(input)
+    validate_numbers(numbers)
     valid_numbers = numbers.filter{ |number| number <= 1000}
     valid_numbers.sum
   end
 
   private
 
-  def self.parse_add_input(input)
-    return parse_add_input_delimited(input) if input.start_with?('//')
-    input_with_comma_separator = input.gsub('\n', ',')
-    str_to_int_array_by_delimiter(input_with_comma_separator, ',' )
+  def self.parse_input(input)
+    numbers_string = input
+    delimiters = ['\n', ',']
+    if input.start_with?('//')
+      delimiter_string, numbers_string = input.split('\n')
+      delimiters = get_delimiters(delimiter_string)
+    end 
+    str_to_int_array_by_delimiter(numbers_string, delimiters)
   end
 
-  def self.parse_add_input_delimited(input)
-    delimiter_string, numbers_string = input.split('\n')
+  def self.get_delimiters(delimiter_string)
     delimiter_string_without_prefix = delimiter_string[2..]
     delimiters = [delimiter_string_without_prefix]
     delimiters = delimiter_string_without_prefix.scan(/\[(.)\]/).flatten if delimiter_string_without_prefix.start_with?('[')
-    str_to_int_array_by_delimiter(numbers_string, delimiters )
+    delimiters
   end
 
   def self.str_to_int_array_by_delimiter(string, delimiters)
@@ -28,7 +31,7 @@ module StringCalculator
     tokens.map { |token| token.strip.to_i}
   end
 
-  def self.validate_add_numbers(numbers)
+  def self.validate_numbers(numbers)
     negative_numbers = numbers.filter{ |number| number < 0 }
     return if negative_numbers.empty?
     raise ArgumentError.new("negative numbers not allowed <#{negative_numbers.join(' ')}>")
