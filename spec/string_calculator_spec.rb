f51f233bdedd3d9d1d677a8ac1e0faa7c5918590
Wizzenalum <sikri.ghanshyam@gmail.com>
2025-04-03 23:10:59 +0530

    support input with single integer

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe StringCalculator do
         expect(described_class.add(input)).to be(0)
       end
     end
+
+    context 'when input string has single integer number' do
+      let(:input) {'10'}
+      it 'return the number' do
+        expect(described_class.add(input)).to be(10)
+      end
+    end
   end
 end
