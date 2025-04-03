893bb22ebdd62a06d9075e1e2604928e7d767981
Wizzenalum <sikri.ghanshyam@gmail.com>
2025-04-03 23:34:41 +0530

    modify add to support multiple comma separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,7 @@ module StringCalculator
   def self.add(input)
     return 0 if input.empty?
     tokens = input.split(',')
-    first_number, second_number = tokens[0].to_i, tokens[1]&.to_i || 0
-    first_number + second_number
+    numbers = tokens.map { |token| token.to_i}
+    numbers.sum
   end
 end
